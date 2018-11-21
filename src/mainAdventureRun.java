cab948c5d649f334276190f9d7a0600ff132dda8
Satato <amber.melton.2020@gmail.com>
2018-11-21 16:51:50 -0500

    New Code Edit #1
    added to initial introduction;
    still haven't done any character creation aside from getting username;
    prepared hendrickLore() method for development.

--- src/mainAdventureRun.java
+++ src/mainAdventureRun.java
@@ -19,18 +19,19 @@ public class mainAdventureRun
 	//game opening; contains initial dialogue and character set-up.
 	public static void main(String[] args)
 	{
-		System.out.println("Hello there!\nWhat's your name, adventurer?\n");
+		System.out.println("Hello there!\nWhat's your name, adventurer?");
 		username = scan.nextLine();
-		System.out.println("Wow..."+username+"...how valiant a name!");
+		System.out.println("\nWow..."+username+"...how valiant a name!");
 		System.out.println("I'm Hendrick. Nice to meet you, "+username);
 		System.out.println("I'd ask what brings you here...but the answer is me!\nhaha!");
-		System.out.println("\n*laugh or stay silent?*\n");
+		System.out.println("\n*'laugh' or 'stay silent'?*\n");
 		String userResponse = scan.nextLine();
 		
 		//ensures that the user chooses one of the options by repeating the question until they do.
 		while(!(userResponse.equals("laugh")||userResponse.equals("stay silent")))
 		{
 			System.out.println("Invalid response.\n*Laugh or stay silent?*\n");
+			userResponse = scan.nextLine();
 		}
 		//gives appropriate response based on player's choice
 		if(userResponse.equals("laugh"))
@@ -40,7 +41,38 @@ public class mainAdventureRun
 		
 		System.out.println("Well, "+username+", welcome to the village of Cnetha!");
 		System.out.println("We've heard stories of your valiance and chivalry...we want to make you our Knight");
+		System.out.println("\n*'yes' or 'no thank you'*\n");
+		userResponse = scan.nextLine();
 		
+		//makes sure the player chooses yes or no
+		while(!(userResponse.equals("yes")||userResponse.equals("no thank you")))
+		{
+			System.out.println("Invalid response.\n*'yes' or 'no thank you'*\n");
+			userResponse = scan.nextLine();
+		}
+		//determines appropriate response
+		if(userResponse.equals("yes"))
+			System.out.println("Perfect!");
+		else if(userResponse.equals("no"))
+			System.out.println("Well, uh, it's sort of the...erm...King's orders?\nI'm not sure you really have a choice in the matter...");
+		
+		System.out.println("Let's get you ready then, shall we?");
+		System.out.println("*let's go*");
+		userResponse = scan.nextLine();
+		
+		//waits on player to agree
+		while(!userResponse.equals("let's go"))
+		{
+			System.out.println("Invalid response.\n*let's go*");
+			userResponse = scan.nextLine();
+		}
+		
+		System.out.println("Let me tell you the story of our village...");
+	}
+	
+	//new method for Hendrick to tell the user world lore
+	public static void hendrickLore()
+	{
 		
 	}
 
